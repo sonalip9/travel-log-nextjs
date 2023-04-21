c5d8bb94d9df1d5a678a61a55fe4c03d46d76224
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-21 12:45:08 +0530

    chore(ui): add NextUI as UI kit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react';
+import type { AppProps } from 'next/app';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+import { darkTheme, lightTheme } from '@styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      value={{
+        dark: darkTheme.className,
+        light: lightTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
