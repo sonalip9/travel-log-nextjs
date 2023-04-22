fe6b6f03bd3f40e7a3ee6d2565e8df030d2f96da
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-22 22:50:05 +0530

    chore: add auth token api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,61 +1,69 @@
 import { AxiosError } from 'axios';
+import { NextApiResponse, NextApiRequest } from 'next';
 import NextAuth, { User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { signIn } from '@/service/api';
 
-export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      id: 'credentials',
-      type: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return await NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        id: 'credentials',
+        type: 'credentials',
+        name: 'Credentials',
+        credentials: {
+          username: { label: 'Email', type: 'text' },
+          password: { label: 'Password', type: 'password' },
+        },
+        authorize: async (credentials) => {
+          try {
+            if (!credentials) return null;
+
+            const response = await signIn(credentials);
+
+            if (response.data) {
+              return {
+                id: response.data.user.id,
+                email: response.data.user.username,
+                accessToken: response.data.accessToken,
+              } as User & { accessToken: string };
+            }
+            return null;
+          } catch (err) {
+            let errorMessage = 'An error occurred';
+            if (err instanceof AxiosError) {
+              errorMessage = err.message;
+            }
+            // Redirecting to the login page with error message in the URL
+            throw new Error(`${errorMessage} & email=${credentials?.username ?? ''}`);
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      session({ session, token }) {
+        session.accessToken = token.accessToken as string;
+
+        return session;
       },
-      authorize: async (credentials) => {
-        try {
-          if (!credentials) return null;
-
-          const res = await signIn(credentials);
-
-          if (res.data) {
-            return {
-              id: res.data.user.id,
-              email: res.data.user.username,
-              accessToken: res.data.accessToken,
-            } as User & { accessToken: string };
-          }
-          return null;
-        } catch (err) {
-          let errorMessage = 'An error occurred';
-          if (err instanceof AxiosError) {
-            errorMessage = err.message;
-          }
-          // Redirecting to the login page with error message in the URL
-          throw new Error(`${errorMessage} & email=${credentials?.username ?? ''}`);
+      jwt({ token, user }) {
+        if (req.url?.endsWith('session?update')) {
+          const { accessToken } = req.headers;
+          token.accessToken = accessToken;
         }
+        if (user && 'accessToken' in user && user.accessToken) {
+          token.accessToken = user.accessToken;
+        }
+        return token;
       },
-    }),
-  ],
-  callbacks: {
-    session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-
-      return session;
     },
-    jwt({ token, user }) {
-      if (user && 'accessToken' in user && user?.accessToken) {
-        token.accessToken = user.accessToken;
-      }
-      return token;
+    pages: {
+      // Changing the error redirect page to our custom login page
+      error: '/login',
+      signIn: '/login',
     },
-  },
-  pages: {
-    // Changing the error redirect page to our custom login page
-    error: '/login',
-    signIn: '/login',
-  },
-  debug: true,
-});
+    debug: true,
+  });
+}
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,14 +1,55 @@
-import axios from 'axios';
-import { LoginPayload, UserRes } from '@defs/auth';
+import axios, { AxiosError } from 'axios';
+import { getSession } from 'next-auth/react';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001',
-  headers: {
-    accept: `*//*`,
-    'Content-Type': 'application/json',
-  },
+import { LoginPayload, UserRes } from '@defs/auth';
+const config = { baseURL: 'http://localhost:3001' };
+
+const api = axios.create(config);
+
+const authAPI = axios.create(config);
+
+authAPI.interceptors.request.use(async (conf) => {
+  try {
+    const session = await getSession();
+
+    if (session?.accessToken) {
+      conf.headers.Authorization = `Bearer ${session?.accessToken}`;
+    }
+    return Promise.resolve(conf);
+  } catch (err) {
+    return Promise.reject(Error('Error in request interceptors', { cause: err }));
+  }
 });
 
+authAPI.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    try {
+      if (error instanceof AxiosError && error.config && error.response?.status === 401) {
+        if (error.config.url?.endsWith('refresh')) {
+          return Promise.reject(error);
+        }
+        const res = await refreshAuth();
+
+        if (res.status === 200) {
+          await axios.get('/api/auth/session?update', {
+            headers: { accessToken: res.data.accessToken },
+          });
+
+          error.config.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+          return authAPI.request(error.config);
+        }
+      }
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  },
+);
+
+//  ======== Auth API ========
 export const signIn = (credentials: LoginPayload) => api.post<UserRes>('/auth/login', credentials);
 
 export const signUp = (credentials: LoginPayload) => api.post<UserRes>('/auth/signup', credentials);
+
+const refreshAuth = async () => authAPI.get<UserRes>('auth/refresh');
