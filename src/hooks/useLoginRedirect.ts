a781283866eec333f588b3dee78f2454db0a5a3c
Sonali Parekh <sonali.parekh912@gmail.com>
2023-04-22 23:48:07 +0530

    refactor: update unauth user redirection

--- src/hooks/useLoginRedirect.ts
+++ src/hooks/useLoginRedirect.ts
@@ -1,14 +1,12 @@
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 
 export const useLoginRedirect = () => {
   const router = useRouter();
-  const { status, data } = useSession();
-
-  useEffect(() => {
-    if (!data?.user.email && status === 'unauthenticated') {
+  useSession({
+    required: true,
+    onUnauthenticated() {
       router.replace('/login').catch((err) => console.error('Redirected:', err));
-    }
-  }, [data?.user, router, status]);
+    },
+  });
 };
