8dcae962b0393459f17d14831826504a518bf30a
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-09 21:43:47 +0530

    style: add values to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,35 @@
 import { createTheme, useTheme } from '@nextui-org/react';
+import { BaseTheme } from '@nextui-org/react/types/theme';
+
+import { darkColors, lightColors } from './palette';
+import { borderRadii, spacing } from './spacing';
+
+const theme: BaseTheme = {
+  colors: lightColors,
+  space: spacing,
+  radii: borderRadii,
+  fonts: {
+    display: 'Courgette, sans-serif',
+    headline: 'Nunito Sans, sans-serif',
+    title: 'Nunito Sans, sans-serif',
+    label: 'Nunito Sans, sans-serif',
+    body: 'Nunito Sans, sans-serif',
+  },
+};
 
 export const lightTheme = createTheme({
   className: 'LightTheme',
   type: 'light',
+  theme,
 });
 
 export const darkTheme = createTheme({
   className: 'DarkTheme',
   type: 'dark',
+  theme: {
+    ...theme,
+    colors: darkColors,
+  },
 });
 
 export const useAppTheme = useTheme;
