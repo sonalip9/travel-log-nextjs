8ef1a686bce5fce60263f197d819516be43aebfd
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-16 22:13:27 +0530

    feat(journals): create or update a journal

--- src/components/templates/Journals/index.ts
+++ src/components/templates/Journals/index.ts
@@ -1,2 +1,4 @@
+export { default as EditJournalModal } from './EditJournalModal';
 export { default as Journals } from './Journals';
 export type { JournalsProps } from './Journals';
+export type { EditJournalModalProps, CreateProps, UpdateProps } from './EditJournalModal';
