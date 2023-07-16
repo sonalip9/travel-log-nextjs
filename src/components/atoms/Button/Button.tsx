6c46c84ca9c6a852c977ff29393e997e0b057365
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-16 22:04:19 +0530

    chore(component): overwrite default variants
    To use the colors from the theme declared, overwrite the default
    variant values of button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,16 +1,39 @@
-import { Button, ButtonProps as NUIButtonProps } from '@nextui-org/react';
+import { Button, ButtonProps as NUIButtonProps, styled } from '@nextui-org/react';
 
 import { Loading } from '@components/Loading';
 
-export type ButtonProps = NUIButtonProps & {
+export type ButtonProps = Omit<NUIButtonProps, 'color'> & {
+  color?: 'default' | 'primary';
   isLoading?: boolean;
 };
 
-function ButtonComponent({ isLoading, ...props }: ButtonProps) {
+function ButtonComponent({ isLoading, children, ...props }: ButtonProps) {
+  const StyledButton = styled(Button, {
+    variants: {
+      color: {
+        default: { bg: '$primaryContainer', color: '$onPrimaryContainer' },
+        primary: { bg: '$primaryContainer', color: '$onPrimaryContainer' },
+      },
+      light: {
+        true: { bg: '$transparent', color: '$text' },
+      },
+    },
+    compoundVariants: [
+      {
+        color: 'primary',
+        light: true,
+        css: { bg: '$transparent', color: '$primary' },
+      },
+    ],
+    defaultVariants: {
+      color: 'default',
+    },
+  });
+
   return (
-    <Button {...props}>
-      {isLoading ? <Loading color="currentColor" size="sm" type="default" /> : props.children}
-    </Button>
+    <StyledButton {...props}>
+      {isLoading ? <Loading color="currentColor" size="sm" type="default" /> : children}
+    </StyledButton>
   );
 }
 
