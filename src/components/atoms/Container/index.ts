8f4b33d42f40522a6e468c0beecb72a4b3ad0c44
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-22 23:39:45 +0530

    chore(ui): create column component

--- src/components/atoms/Container/index.ts
+++ src/components/atoms/Container/index.ts
@@ -1,2 +1,4 @@
 export { default as Container } from './Container';
 export type { ContainerProps } from './Container';
+export { default as Column } from './Column';
+export type { ColumnProps } from './Column';
