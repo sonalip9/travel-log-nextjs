10edfb0c43c13d2807d2a7d030aa2796909aa5db
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-29 22:27:14 +0530

    chore: create switch component

---
+++ src/components/atoms/Switch/Switch.tsx
@@ -0,0 +1,9 @@
+import { Switch as NUISwitch, SwitchProps as NUISwitchProps } from '@nextui-org/react';
+
+export type SwitchProps = NUISwitchProps;
+
+function Switch(props: SwitchProps) {
+  return <NUISwitch {...props} />;
+}
+
+export default Switch;
