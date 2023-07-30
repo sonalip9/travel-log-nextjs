1f91441c047440dd1f930ebb2e28a22f954bca35
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-30 11:20:57 +0530

    chore: create nav bar component

---
+++ src/components/templates/NavBar/NavBar.tsx
@@ -0,0 +1,50 @@
+import { signOut, useSession } from 'next-auth/react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@components/Button';
+import { Container } from '@components/Container';
+import { Switch } from '@components/Switch';
+import { MoonIcon, SunIcon } from '@icons';
+import { useAppTheme } from '@styles/theme';
+
+function NavBar() {
+  const { setTheme } = useTheme();
+  const { isDark } = useAppTheme();
+
+  const { status } = useSession();
+
+  return (
+    <Container
+      alignCenter
+      flex
+      row
+      css={{ p: '$sm', gap: '$lg', width: 'auto' }}
+      justify="flex-end"
+      responsive={false}
+    >
+      <Switch
+        checked={!isDark}
+        iconOff={<MoonIcon filled />}
+        iconOn={<SunIcon filled />}
+        size="xl"
+        onChange={({ target }) => setTheme(target.checked ? 'light' : 'dark')}
+      />
+
+      {status === 'authenticated' && (
+        <Button
+          auto
+          bordered
+          color="primary"
+          css={{ textTransform: 'uppercase' }}
+          onPress={() => {
+            signOut({ callbackUrl: '/login', redirect: true }).catch(console.error);
+          }}
+        >
+          Sign Out
+        </Button>
+      )}
+    </Container>
+  );
+}
+
+export default NavBar;
