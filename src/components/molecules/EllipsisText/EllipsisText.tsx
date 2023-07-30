791699ff89147f0e6b3ff25f721aeee11963bd25
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-30 17:56:13 +0530

    chore: improve logic of ellipsis

--- src/components/molecules/EllipsisText/EllipsisText.tsx
+++ src/components/molecules/EllipsisText/EllipsisText.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 import { Container, ContainerProps } from '@components/Container';
 import { Text, TextProps } from '@components/Text';
@@ -11,8 +11,6 @@ function EllipsisText({ containerStyle, ...textProps }: EllipsisTextProps) {
   const textRef = useRef<HTMLElement>(null);
   const containerRef = useRef<HTMLElement>(null);
 
-  const [text, setText] = useState(textRef.current?.innerText);
-
   useLayoutEffect(() => {
     const currText = textRef.current?.innerText;
     if (!currText) return;
@@ -20,34 +18,47 @@ function EllipsisText({ containerStyle, ...textProps }: EllipsisTextProps) {
     const textComputedStyle = window.getComputedStyle(textRef.current);
     const textWidth = textRef.current?.offsetWidth || 0;
     const textHeight = textRef.current?.offsetHeight || 0;
-    const lineHeight = textComputedStyle.getPropertyValue('line-height').replace('px', '');
+    const lineHeight = parseInt(
+      textComputedStyle.getPropertyValue('line-height').replace('px', ''),
+    );
 
     const containerWidth = containerRef.current?.offsetWidth || 0;
     const containerHeight = containerRef.current?.offsetHeight || 0;
 
     if (textWidth <= containerWidth && textHeight <= containerHeight) return;
 
-    const lineCount = Math.floor(containerHeight / parseInt(lineHeight));
-    const currLines = Math.floor(textHeight / parseInt(lineHeight));
+    const lineCount = Math.floor(containerHeight / lineHeight);
+    const currLines = Math.floor(textHeight / lineHeight);
 
-    const charInLine = currText.length / currLines;
+    let charInLine = Math.floor(currText.length / currLines);
+    charInLine = currText.indexOf(' ', charInLine);
 
-    // Truncate all words that are beyond the limit
-    // Add ellipsis to the last word
-    const truncatedText = textRef.current?.innerText.split(' ').reduce((acc, word) => {
-      if (acc.endsWith('...')) return acc;
+    textRef.current.innerText = currText.slice(0, charInLine);
 
-      return acc.length + word.length + 4 > charInLine * lineCount
-        ? `${acc} ...`
-        : `${acc} ${word}`;
-    }, '');
-    setText(truncatedText || '');
+    // Find the number of characters that fit in the first line
+    while (textRef.current.offsetHeight / lineHeight <= 1) {
+      textRef.current.innerText += currText[charInLine];
+      charInLine += 1;
+    }
+
+    // Add the total characters that fit in the `lineCount` lines
+    textRef.current.innerText = currText.slice(0, charInLine * lineCount);
+
+    // Remove words until the text fits in the container
+    if (textRef.current.offsetHeight > containerHeight) {
+      while (textRef.current.offsetHeight > containerHeight) {
+        textRef.current.innerText = textRef.current.innerText.split(' ').slice(0, -1).join(' ');
+      }
+    }
+
+    // Add ellipsis
+    textRef.current.innerText = textRef.current.innerText + '...';
   }, []);
 
   return (
     <Container ref={containerRef} css={containerStyle}>
-      <Text ref={textRef} {...textProps}>
-        {text || textProps.children}
+      <Text ref={textRef} {...textProps} css={{ whiteSpace: 'pre-wrap', ...textProps.css }}>
+        {textProps.children}
       </Text>
     </Container>
   );
