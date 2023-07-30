0274e651ff068f0a79a44152d9d44a6df51db2a5
Sonali Parekh <sonali.parekh912@gmail.com>
2023-07-30 11:20:57 +0530

    chore: create nav bar component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,4 +2,6 @@ export { default as ViewOutline } from './ViewOutline';
 export { default as AddCircleOutline } from './AddCircleOutline';
 export { default as DeleteOutline } from './DeleteOutline';
 export { default as EditOutline } from './EditOutline';
+export { SunIcon } from './Sun';
+export { MoonIcon } from './Moon';
 export type { IconProps } from './types';
--- src/components/icons/types.ts
+++ src/components/icons/types.ts
@@ -1,6 +1,7 @@
 export type IconProps = {
   fill?: string;
+  filled?: boolean;
   size?: number;
   height?: number;
   width?: number;
-};
+} & React.SVGProps<SVGSVGElement>;
