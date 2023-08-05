1d34833ed96f8c394a1e81f86b8fb963e1f892cf
Sonali Parekh <sonali.parekh912@gmail.com>
2023-08-05 20:35:06 +0530

    fix: incorrect svg props

--- src/components/icons/DeleteOutline.tsx
+++ src/components/icons/DeleteOutline.tsx
@@ -11,10 +11,10 @@ function DeleteOutline({ fill = 'currentColor', size, height, width, ...props }:
       {...props}
     >
       <path
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M15 3V4H20V6H19V19C19 20.1 18.1 21 17 21H7C5.9 21 5 20.1 5 19V6H4V4H9V3H15ZM7 19H17V6H7V19ZM9 8H11V17H9V8ZM15 8H13V17H15V8Z"
         fill={fill}
-        fill-rule="evenodd"
+        fillRule="evenodd"
       />
     </svg>
   );
